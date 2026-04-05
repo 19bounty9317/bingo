e005ca378ee23711b714202f2c736fbf70642270
19bounty9317 <pokego-9317@web.de>
2026-04-05 10:02:05 +0200

    Add Firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,10 @@
-// Firebase Konfiguration für Rocket League Bingo
-// Diese Config wird von index.html importiert
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyBGffa3OQOvZZgWkJxd_MxbWOAR49yflPc",
-  authDomain: "rl-bingo-465a6.firebaseapp.com",
-  databaseURL: "https://rl-bingo-465a6-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "rl-bingo-465a6",
-  storageBucket: "rl-bingo-465a6.firebasestorage.app",
-  messagingSenderId: "1066602059596",
-  appId: "1:1066602059596:web:dd9ea89513b18e5631a1e9",
-  measurementId: "G-RMJK2WD9WY"
+    apiKey: "AIzaSyBGffa3OQOvZZgWkJxd_MxbWOAR49yflPc",
+    authDomain: "rl-bingo-465a6.firebaseapp.com",
+    databaseURL: "https://rl-bingo-465a6-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "rl-bingo-465a6",
+    storageBucket: "rl-bingo-465a6.firebasestorage.app",
+    messagingSenderId: "1066602059596",
+    appId: "1:1066602059596:web:dd9ea89513b18e5631a1e9",
+    measurementId: "G-RMJK2WD9WY"
 };
